6e07bdffa5a91c38459958749227e9af01c0deff
Pedro <pedrotenerelli257@gmail.com>
2023-11-23 13:05:11 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,9 @@
+const email = document.getElementById('email');
+const senha = document.getElementById('senha');
+const submit = document.getElementsByClassName('form')[0];
+
+
+submit.addEventListener('submit',(e)=>{
+    e.preventDefault();
+
+})
