aa6afe88151de99c492fa08947836ccf5b127cf2
Pedro <pedrotenerelli257@gmail.com>
2023-11-23 13:13:01 -0300

    fix

--- script.js
+++ script.js
@@ -6,4 +6,15 @@ const submit = document.getElementsByClassName('form')[0];
 submit.addEventListener('submit',(e)=>{
     e.preventDefault();
 
+
+    Email.send({
+        SecureToken : "767def94-feeb-4465-b435-c989fe0fa25f",
+        To : 'pedrotenerelli257@gmail.com',
+        From : "pedrotenerelli257@gmail.com",
+        Subject : "Teste",
+        Body : "Teste"
+    }).then(
+      message => alert(message)
+    );
+
 })
